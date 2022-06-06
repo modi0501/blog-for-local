9b61aa44648cf7e6bbe0aa2acccb94e23d60d092
modi0501 <mmayank0501@gmail.com>
2022-06-06 11:54:01 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import Home from "./components/Home/Home";
+import CreateBlog from "./components/CreateBlog/CreateBlog";
+import Blog from "./components/Blog/Blog";
+import Signup from "./components/Signup/Signup";
+import Signin from "./components/Signin/Signin";
+import { isLoggedIn } from "./utils/auth";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="create" element={<CreateBlog />} />
+      <Route path="blog/:id" element={<Blog />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ server/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import * as jwt from "jsonwebtoken";
+import schema from "./graphql/schemasMap";
+import User from "./api/user/user.model";
+import "./lib/mongoose";
+import dotenv from "dotenv";
+import cors from 'cors';
+
+const PORT = 4000;
+
+const app = express();
+app.use(cors())
+dotenv.config();
+const server = new ApolloServer({
+  schema,
+  context: async ({ req }) => {
+    const token = req.headers.authorization || "";
+    let user;
+
+    if (token) {
+      const decode: any = await jwt.verify(token, process.env.TOKEN_KEY!);
+      user = await User.findOne({ _id: decode._id }).lean();
+
+      if (!user) {
+        throw new Error("UnAuthorized");
+      }
+    }
+    
+    return { user };
+  },
+});
+server.applyMiddleware({ app, path: "/graphql" });
+app.listen(PORT, () => {
+  console.log(
+    `\n🚀      GraphQL is now running on http://localhost:${PORT}/graphql`
+  );
+});
